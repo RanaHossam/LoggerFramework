b5e53cd0404c8794a38aa99176e7056966f8411c
Rana Hossam <ranahossam263@gmail.com>
2020-01-30 15:08:26 +0200

    add files

---
+++ LoggerFramework/LoggerFramework.framework/Headers/LoggerFramework.h
@@ -0,0 +1,19 @@
+//
+//  LoggerFramework.h
+//  LoggerFramework
+//
+//  Created by Rana Hossam on 1/30/20.
+//  Copyright © 2020 Rana Hossam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoggerFramework.
+FOUNDATION_EXPORT double LoggerFrameworkVersionNumber;
+
+//! Project version string for LoggerFramework.
+FOUNDATION_EXPORT const unsigned char LoggerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoggerFramework/PublicHeader.h>
+
+
